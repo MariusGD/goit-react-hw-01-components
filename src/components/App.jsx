8d7b4b892ccbe4e2574e5275178d6353811a0cd9
MariusGD <gagumarius2015@gmail.com>
2023-11-17 15:54:15 +0200

    update2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import Friend from './components/Friend/FriendList';
+import Transactions from './components/Transactions/Transactions';
+
+import user from './components/Data/user.json';
+import data from './components/Data/data.json';
+import friends from './components/Data/friends.json';
+import transactions from './components/Data/transactions.json';
+
+import styles from './App.module.css';
+
+export function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles.app}>
+      {' '}
+      <h1 className={styles.title}>React Homework 1 - Components</h1>
+      <div className={styles.components}>
+        <Profile user={user} />
+        <Statistics stats={data} />
+        <Friend friends={friends} />
+        <Transactions items={transactions} />
+      </div>
     </div>
   );
-};
+}
+
+export default App;
